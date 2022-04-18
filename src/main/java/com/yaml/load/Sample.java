ac2d62b75f8d347bcfcaf53664c25c304879aad8
pradhan <techbulletinprime@gmail.com>
2022-04-18 12:17:11 +0530

    commit 2

--- src/main/java/com/yaml/load/Sample.java
+++ src/main/java/com/yaml/load/Sample.java
@@ -2,6 +2,7 @@ package com.yaml.load;
 
 public class Sample {
     public static void main(String[] args) {
+        System.out.println("hi all");
         System.out.println("hi, how are you all ?");
     }
 }
