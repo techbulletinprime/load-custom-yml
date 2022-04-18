303da8970448aed5ff35fd9c1b92822dcc4891c8
techbulletinprime <67306841+techbulletinprime@users.noreply.github.com>
2022-04-18 12:09:35 +0530

    commit 1

--- src/main/java/com/yaml/load/Sample.java
+++ src/main/java/com/yaml/load/Sample.java
@@ -2,6 +2,6 @@ package com.yaml.load;
 
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("hi, how are you all ?");
     }
 }
